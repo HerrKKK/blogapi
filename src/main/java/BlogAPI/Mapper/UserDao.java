6f3ae5dfbd4bdaffc563c60ad87c832b5b814d57
wang.weiran <wang.wei.ran@outlook.com>
2022-10-11 05:35:26 +1100

    add userDao

---
+++ src/main/java/BlogAPI/Mapper/UserDao.java
@@ -0,0 +1,12 @@
+package BlogAPI.Mapper;
+
+import BlogAPI.Entity.SysUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends JpaRepository<SysUser, Long>,
+                                 JpaSpecificationExecutor<SysUser> {
+    public SysUser findByUserName(String userName);
+}
