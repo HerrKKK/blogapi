0ca94bb7068eafba95aa2ff1fb557c7dcf8abec6
wang.weiran <wang.wei.ran@outlook.com>
2022-10-10 23:21:34 +1100

    add tags

--- src/main/java/BlogAPI/Entity/Content.java
+++ src/main/java/BlogAPI/Entity/Content.java
@@ -4,7 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Setter
@@ -16,4 +19,6 @@ public class Content extends Folder {
     private String url;
     @OneToOne
     private SysUser author;
+    @ManyToMany
+    Set<Tag> tags = new HashSet<>();
 }
---
+++ src/main/java/BlogAPI/Entity/Tag.java
@@ -0,0 +1,22 @@
+package BlogAPI.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+
+    @ManyToMany(mappedBy = "tags")
+    Set<Content> articles = new HashSet<>();
+}
