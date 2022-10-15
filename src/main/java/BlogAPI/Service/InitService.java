e44d8d405066c675d313648c78fadd8728e8e8e7
wang.weiran <wang.wei.ran@outlook.com>
2022-10-15 13:28:43 +1100

    add role

--- src/main/java/BlogAPI/Service/InitService.java
+++ src/main/java/BlogAPI/Service/InitService.java
@@ -1,5 +1,6 @@
 package BlogAPI.Service;
 
+import BlogAPI.Entity.SysRole;
 import BlogAPI.Entity.SysUser;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,22 +9,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class InitService implements ApplicationRunner {
     private final UserService userService;
-
-    public InitService(UserService userService) {
+    private final RoleService roleService;
+    public InitService(UserService userService,
+                       RoleService roleService) {
         this.userService = userService;
+        this.roleService = roleService;
     }
-
     @Override
     public void run(ApplicationArguments args) {
         System.out.println("boot up");
         addAdmin();
     }
-
     public void addAdmin() {
         var admin = new SysUser();
         admin.setUserName("wwr");
         admin.setEmail("iswangwr@gmail.com");
         admin.setPwdHash("153226");
+        var role = new SysRole();
+        role.setName("admin");
+
+        try {
+            role = roleService.newRole(role);
+            admin.getRoles().add(role);
+        } catch (Exception e) {
+            System.out.println("admin role existed");
+        }
         try {
             userService.addUser(admin);
         } catch (Exception e) {
