90ecd5857ebe67c48149a065296bb6114b10663d
wang.weiran <wang.wei.ran@outlook.com>
2022-10-31 14:17:22 +1100

    ignore subResources

--- src/main/java/BlogAPI/Entity/Resource.java
+++ src/main/java/BlogAPI/Entity/Resource.java
@@ -1,5 +1,6 @@
 package BlogAPI.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -14,6 +15,7 @@ import java.util.Set;
 @ToString(exclude = {"parent"})
 @EqualsAndHashCode(exclude = {"parent"})
 @Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true, value = {"subResources"})
 public class Resource {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,7 +32,7 @@ public class Resource {
     private Resource parent;
     @OneToMany(mappedBy="parent",
                cascade=CascadeType.REMOVE,
-               fetch=FetchType.EAGER)
+               fetch=FetchType.LAZY)
     private Set<Resource> subResources = new HashSet<>();
 
     @ManyToMany(fetch = FetchType.EAGER)
